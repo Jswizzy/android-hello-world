d24f5ddae29753c8b8037f1ae0ff5b30cf627495
Jswizzy <smithjustinn@hotmail.com>
2017-08-05 19:08:35 -0400

    add layout experiments

---
+++ app/src/main/java/com/sqisland/android/hello/LayoutFundamentals.java
@@ -0,0 +1,14 @@
+package com.sqisland.android.hello;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class LayoutFundamentals extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layoutFundamentals);
+    }
+
+}
